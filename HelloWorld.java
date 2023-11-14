afd825144c371e50f795ec341ddc501fe4e767bf
Jsaulbautista <150806077+Jsaulbautista@users.noreply.github.com>
2023-11-14 09:43:35 -0500

    Update HelloWorld.java
    quite el nombre

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("3625");
 		System.out.println("cdrfghnj");
-		System.out.println("hola franchesco");
+		System.out.println("holaaaaaaa");
 	}
 }
