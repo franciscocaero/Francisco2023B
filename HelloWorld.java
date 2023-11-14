7935054d06e13509068fa41b559e6324ca07cba0
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2023-11-14 09:08:02 -0500

    Update HelloWorld.java
    probar cambios

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello");
-		System.out.println("ZZZ---");
+		System.out.println("3625");
+		System.out.println("cdrfghnj");
 	}
 }
